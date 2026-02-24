8e2a1abb4e145ab56ad62059398b5db21f0c25db
Bareddy Reshnavi Devi <bareddyreshnavidevi@gmail.com>
2026-02-24 07:49:28 +0530

    Remove Badge import from HomePage component

--- farm-value-app/src/pages/HomePage.jsx
+++ farm-value-app/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Users, TrendingUp, Truck, Shield, ShoppingCart, CheckCircle } from 'lucide-react';
 import { useCart } from '../context/CartContext';
 import { mockProducts } from '../data/mockData';
-import { Card, Badge } from '../components/UI';
+import { Card } from '../components/UI';
 import Button from '../components/Button';
 import ProductImage from '../components/ProductImage';
 
